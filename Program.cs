079118a931552dd69d7e6da6ea2dd0ef4a3d78ae
Arve Svendsen <arve.svendsen@gmail.com>
2022-02-09 20:22:20 +0100

    Cloud Run port

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var url = $"http://0.0.0.0:{port}";
+var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+
+
+app.MapGet("/", () => "Hello {target}!");
+
+app.Run(url);
